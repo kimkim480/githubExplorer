50a91c0204ba8a8a353f0d6ae413945425e0bcef
Eric Alves <eric@pop-os.localdomain>
2020-11-21 17:17:44 -0300

    github explorer

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: 'https://api.github.com',
   auth: {
     username: 'kimkim480',
-    password: '6c4f454f3edab811052649fb854a45c53de0e456',
+    password: `${process.env.REACT_APP_GITHUB_TOKEN}`,
   },
 });
 
